2f712fbaabc4880b4a22f1f56b058e63b5e9431e
kseyko35 <seyfiercan35@hotmail.com>
2020-05-20 22:36:26 +0300

    zxing library is added

---
+++ QrCodeExample/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace QrCodeExample
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void  Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var scan = new ZXingScannerPage();
+            await Navigation.PushAsync(scan);
+            scan.OnScanResult += (result ) =>
+            {
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await Navigation.PopAsync();
+                    Console.Write(result);
+                    myCode.Text = result.Text;
+                });
+            };
+        }
+
+       
+    }
+}
